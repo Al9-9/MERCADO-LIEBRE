e7a5f0b6612bc08f39317b189b0298b15eac37f1
Al <aldananunez663@gmail.com>
2024-03-10 22:24:53 -0300

    Agregué otra funcionalidad al controlador destroy

--- src/controllers/admin/destroy.controller.js
+++ src/controllers/admin/destroy.controller.js
@@ -1,12 +1,19 @@
 const { readData, saveData } = require('../../data')
-
-
+const path = require('path')
+const fs = require('fs')
 
 module.exports = (req,res) => {
     const {id} = req.params;
     const products = readData()
 
     const  productDelete = products.filter (p => p.id !== +id)
+    const productDestroy = products.find(p => p.id === +id)
+
+    const pathFile = path.join(__dirname, '../../../public/images/products' + productDestroy.image)
+    const existFile = fs.existsSync(pathFile)
+    if(existFile){
+        fs.unlinkSync(pathFile)
+    }
 
     saveData(productDelete)
     res.redirect('/')
